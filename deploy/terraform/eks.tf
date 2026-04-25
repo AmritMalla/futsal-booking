576eaff51d9c5fd46b7c7c33e8450b8e8dcdcd14
Amrit Malla <amritmalla09@gmail.com>
2026-04-25 12:52:30 +0545

    feat(infra): EKS cluster + managed node group (2x t3.large, EBS CSI addon)

---
+++ deploy/terraform/eks.tf
@@ -0,0 +1,38 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.24"
+
+  cluster_name    = local.name
+  cluster_version = var.cluster_version
+
+  cluster_endpoint_public_access       = true
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"] # sandbox-only
+
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_addons = {
+    coredns            = { most_recent = true }
+    kube-proxy         = { most_recent = true }
+    vpc-cni            = { most_recent = true }
+    aws-ebs-csi-driver = { most_recent = true }
+  }
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = [var.node_instance_type]
+      ami_type       = "AL2_x86_64"
+
+      min_size     = var.node_desired_size
+      desired_size = var.node_desired_size
+      max_size     = var.node_desired_size + 1
+
+      iam_role_additional_policies = {
+        # Nodes need ECR pull; the managed policy covers it.
+        AmazonEC2ContainerRegistryReadOnly = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+      }
+    }
+  }
+}
